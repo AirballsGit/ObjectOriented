edbd076dc032e8555a5211579a282fc25fc7bb29
Joe Torrance <joe_torrance@yahoo.com>
2022-11-04 17:35:40 -0400

    start practice

--- app.js
+++ app.js
@@ -526,3 +526,16 @@
 // }
 
 //**********************PRACTICE********************** */
+
+function Triangle(a,b){
+    this.a = a; 
+    this.b = b; 
+    this.getArea = function(){
+        return this.a * this.b / 2; 
+    };
+    this.getHypotenuse = function (){
+        return Math.sqrt(this.a**2 + this.b**2);
+    }
+}
+
+const tri1 = new Triangle(3,4);
